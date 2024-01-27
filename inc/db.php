10ff039cdaccc11aeadb130f9ea2d9c4da81fca6
giakhuu <giakhuu18112004@gmail.com>
2024-01-27 15:03:18 +0700

    Giải quyết xung đột và cập nhật code

--- inc/db.php
+++
@@ -1,8 +0,0 @@
-<?
-    require_once "../config.php";
-    require_once "../classes/database.php";
-    function dbconn() {
-        $db = new Databbase(DB_HOST, DB_NAME, DB_USER, DB_PASS);
-        return $db->getConn();
-    }
-?>
--- main.php
+++
@@ -1,4 +0,0 @@
-<?
-    include("./inc/config_session.inc.php");
-    header("Location: view/login.php"); 
-?>
