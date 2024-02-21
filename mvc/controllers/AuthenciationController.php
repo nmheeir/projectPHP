62fccc5385bb53eb2a6039752a3958cf1ed032e4
nmheir <129368081+nmheeir@users.noreply.github.com>
2024-02-21 17:07:50 +0700

    Update Verify Email

--- mvc/controllers/AuthenciationController.php
+++ mvc/controllers/AuthenciationController.php
@@ -1,6 +1,9 @@
 <?
 require_once "../TEST_3/mvc/controllers/BaseController.php";
-class AuthenciationController extends BaseController {
+require_once '../TEST_3/mvc/inc/send_mail.php';
+
+class AuthenciationController extends BaseController
+{
     private $userModel;
     public function __construct()
     {
@@ -8,12 +11,12 @@ class AuthenciationController extends BaseController {
         $this->userModel = new UserModel;
     }
 
-    public function login() {
+    public function login()
+    {
         $this->loadView('frontend.authenciation.login');
-        if(isset($_POST['btnSubmit'])) 
-        { 
+        if (isset($_POST['btnSubmit'])) {
             $username = $_POST['username'];
-            $password = $_POST['password']; 
+            $password = $_POST['password'];
             // lưu dữ liệu khi error
             $sessionLogin = [
                 "username" => $username,
@@ -22,8 +25,7 @@ class AuthenciationController extends BaseController {
             $_SESSION["session_login"] = $sessionLogin;
             // kiểm tra login
             $data = $this->userModel->login($username, $password);
-            if($data->isSuccess)
-            { 
+            if ($data->isSuccess) {
                 // thiết lập session
                 $sessionUserInfo = [
                     "id" => $data->data["id"],
@@ -36,79 +38,133 @@ class AuthenciationController extends BaseController {
                 $cookie_user_id = $data->data["id"];
                 setcookie("user_id", $cookie_user_id, time() + (86400 * 30), "/");
                 // redirect
-                header("Location: http://localhost/Project/TEST_3/User/home"); 
-                exit; 
-            } 
-            else {
+                header("Location: http://localhost/Project/TEST_3/User/home");
+                exit;
+            } else {
                 $_SESSION["error_login"] = $data->message;
-                header("Location: login"); 
+                header("Location: login");
                 exit;
             }
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         if (isset($_COOKIE['user_id'])) {
-            unset($_COOKIE['user_id']); 
-            setcookie('user_id', '', -1, '/'); 
+            unset($_COOKIE['user_id']);
+            setcookie('user_id', '', -1, '/');
         }
-        if(isset($_SESSION["user"])) {
+        if (isset($_SESSION["user"])) {
             unset($_SESSION["user"]);
         }
-        header("Location: login"); 
+        header("Location: login");
     }
 
-    public function register() {
-        if(isset($_POST['btnSubmit'])) 
-        { 
+    public function register()
+    {
+        if (isset($_POST['btnSubmit'])) {
             $username = $_POST['username'];
-            $password = $_POST['password']; 
-            $fullname = $_POST['fullname'];  
-            $company_id = $_POST['company_id']; 
+            $password = $_POST['password'];
+            $email = $_POST['email'];
+            $fullname = $_POST['fullname'];
+            $company_id = $_POST['company_id'];
             // lưu dữ liệu khi error
             $sessionRegister = [
                 "username" => $username,
                 "password" => $password,
+                "email" => $email,
                 "fullname" => $fullname,
                 "company_id" => $company_id
             ];
             $_SESSION["session_register"] = $sessionRegister;
             // Kiểm tra rỗng
-            if(empty($username) || empty($password) || empty($fullname) || empty($company_id)) {
+            if (empty($username) || empty($password) || empty($fullname) || empty($company_id) || empty($email)) {
                 $_SESSION["error_register"] = "Hãy điền đầy đủ thông tin";
-            }
-            else {
+            } else {
                 // Sử dụng hàm registerUser từ class User để đăng ký người dùng mới
-                $checkRegister =  $this->userModel->registerUser($username, $password, $fullname, $company_id);
-                
-                    // Kiểm tra kết quả đăng ký
-                    if ($checkRegister->isSuccess) {
-                        header("Location: login");
-                        unset($_SESSION["error_register"]);
-                        exit;
-                    } else {
-                        $_SESSION["error_register"] = $checkRegister->message;
-                    }
+                $checkRegister =  $this->userModel->checkUser($username, $email);
+
+                // Kiểm tra kết quả đăng ký
+                if ($checkRegister->isSuccess) {
+
+                    $mail = $_SESSION['session_register']['email'];
+                    
+                    $verifyCode = rand(100000, 999999);
+
+                    $subject = "Verify your email";
+                    $message = 'Your verification code is: ' . $verifyCode;
+                    
+                    $sql = "INSERT INTO verify (code, expires, email) 
+                            VALUES ({$verifyCode}, DATE_ADD(NOW(), INTERVAL 60 MINUTE), '{$mail}');";
+
+                    $this->userModel->custom($sql);
+                    
+                    sendMail($mail, $subject, $message);
+
+                    header("Location: verifyemail");
+                    unset($_SESSION["error_register"]);
+                    exit;
+                } else {
+                    $_SESSION["error_register"] = $checkRegister->message;
                 }
             }
-            $this->loadView('frontend.authenciation.register');
         }
-        
-    public function checkLogin() {
-        if(isset($_SESSION["user"])) {
-            return true;
-        }
-        else {
-            if(!isset($_COOKIE["user_id"])) {
-                return false;
+        $this->loadView('frontend.authenciation.register');
+    }
+
+    public function verifyemail()
+    {
+        if (isset($_POST['btnVerify'])) {
+            $verifyCode = $_POST['verifyCode'];
+            
+            //lấy code trong bảng verify dựa vào email
+            $checkCode = $this->userModel->get('verify', [
+                'select' => '*',
+                'where' => "code = '{$verifyCode}' AND email = '{$_SESSION['session_register']['email']}'",
+                'limit' => '1'
+            ]);
+
+            
+            if (!empty($checkCode)) {
+                date_default_timezone_set('Asia/Ho_Chi_Minh');
+                $time = strtotime(date('Y-m-d H:i:s'));
+                $expiresTime = strtotime($checkCode[0]['expires']);
+                if ($verifyCode == $checkCode[0]['code'] && $time < $expiresTime) {
+                    $username = $_SESSION['session_register']['username'];
+                    $password = $_SESSION['session_register']['password'];
+                    $email = $_SESSION['session_register']['email'];
+                    $fullname = $_SESSION['session_register']['fullname'];
+                    $company_id = $_SESSION['session_register']['company_id'];
+                    $this->userModel->registerUser($username, $password, $fullname, $company_id, $email);
+                    // unset($_SESSION['session_register']);
+                    header("Location: login");
+                }
+                else if ($time > $expiresTime) {
+                    echo "Mã quá hạn, vui lòng thử lại mã khác";
+                }
+                else {
+                    echo "Sai mã xác thực, vui lòng nhập lại";
+                }
             }
             else {
+                echo "Vui lòng nhập mã xác thực";
+            }
+        }
+        $this->loadView('frontend.authenciation.verifyemail');
+    }
+    public function checkLogin()
+    {
+        if (isset($_SESSION["user"])) {
+            return true;
+        } else {
+            if (!isset($_COOKIE["user_id"])) {
+                return false;
+            } else {
                 echo $_COOKIE["user_id"];
                 $data = $this->userModel->getUser([
                     'where' => "id = '{$_COOKIE["user_id"]}'"
                 ]);
-                if($data->isSuccess)
-                { 
+                if ($data->isSuccess) {
                     // thiết lập session
                     $sessionUserInfo = [
                         "id" => $data->data[0]["id"],
@@ -118,12 +174,10 @@ class AuthenciationController extends BaseController {
                     ];
                     $_SESSION["user"] = $sessionUserInfo;
                     return true;
-                }
-                else {
+                } else {
                     return false;
                 }
             }
         }
     }
-
 }
