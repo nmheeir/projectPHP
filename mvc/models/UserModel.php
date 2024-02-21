079326b72d6f9f4f8e69fd1c50eb12d3a15b0f8d
nmheir <129368081+nmheeir@users.noreply.github.com>
2024-02-21 17:13:37 +0700

    Thêm hàm checkUser và registerUser

--- mvc/models/UserModel.php
+++ mvc/models/UserModel.php
@@ -1,7 +1,8 @@
 <?
 
 require_once "../TEST_3/mvc/models/BaseModel.php";
-class UserModel extends BaseModel {
+class UserModel extends BaseModel
+{
     private $id;
     private $username;
     private $email;
@@ -15,24 +16,25 @@ class UserModel extends BaseModel {
 
     const TABLE_NAME = "users";
 
-    public function __construct() {
-
+    public function __construct()
+    {
     }
 
     //get data
     //trả về mảng 2 chiều chứa thông tin user, không phải object
-    public function getUser($option = []) {
+    public function getUser($option = [])
+    {
         $user = $this->get(self::TABLE_NAME, $option);
-        if($user) {
+        if ($user) {
             return new DataView(true, $user, "Ok");
-        }
-        else {
+        } else {
             return new DataView(false, $user, "CANNOT FOUND USER");
         }
     }
 
     //get array role
-    public function getRole($role_id) {
+    public function getRole($role_id)
+    {
         if ($this->roleId < $role_id) {
             return $this->get(self::TABLE_NAME, [
                 'where' => 'role_id'
@@ -43,13 +45,15 @@ class UserModel extends BaseModel {
     /**
      * thay đổi hoặc thêm thông tin của user trong database
      */
-    public function saveUser(array $data = []) {
+    public function saveUser(array $data = [])
+    {
         return $this->save(self::TABLE_NAME, $data);
     }
 
-    public function login($username, $password) {
+    public function login($username, $password)
+    {
         $user = $this->getUser([
-            'where' => "username = '{$username}'" 
+            'where' => "username = '{$username}'"
         ]);
         if ($user->isSuccess) {
             $userData = $user->data[0];
@@ -63,19 +67,31 @@ class UserModel extends BaseModel {
             }
         } else {
             return new DataView(false, null, $user->message);
-        }        
+        }
     }
 
-    public function registerUser($username, $password, $fullname, $company_id) {
+    //kiểm tra thông tin trước khi đăng kí
+    public function checkUser($username, $email)
+    {
         $checkIsExitsUsername = $this->getUser(["where" => "username = '{$username}'"]);
-        if($checkIsExitsUsername->isSuccess) {
+        $checkIsExitsEmail = $this->getUser(["where" => "email = '{$email}'"]);
+        if ($checkIsExitsUsername->isSuccess) {
             return new DataView(false, null, "USERNAME đã tồn tại");
         }
+        if ($checkIsExitsEmail->isSuccess) {
+            return new DataView(false, null, "EMAIL đã tồn tại");
+        }
+        return new DataView(true, null, "Ok");
+    }
 
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);      
+    //sau khi verify email thành công -> đăng kí user (đưa dữ liệu vào db )
+    public function registerUser($username, $password, $fullname, $company_id, $email)
+    {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $newUser = [
             'username' => $username,
             'password' => $password,
+            'email' => $email,
             'fullname' => $fullname,
             'company_id' => $company_id,
             'hash_password' => $hashedPassword
@@ -84,8 +100,9 @@ class UserModel extends BaseModel {
         return new DataView(true, null, "Ok");
     }
 
-    function getUserWithRoleAndCompany() {
-        return $this->custom(self::TABLE_NAME, "
+    function getUserWithRoleAndCompany()
+    {
+        return $this->custom("
         SELECT
             u.id AS user_id,
             u.username,
@@ -102,6 +119,5 @@ class UserModel extends BaseModel {
         WHERE
             u.id = 1;
         ");
-
     }
 }
